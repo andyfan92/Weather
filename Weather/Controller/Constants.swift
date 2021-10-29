d01bff36172fa06ee411e6be399f95ceb83382a7
andyfan92 <txair921202@gmail.com>
2021-10-29 10:12:30 -0700

    constant

--- Weather/Controller/Constants.swift
+++ Weather/Controller/Constants.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-//let apiKey = "WGXvLvr3mmG1bUwAfxAJfnZB5AUGMvGK"
-let apiKey = "RVdi5NBIUK9GIdSTYUMGFSKg4GcppCpa"
+let apiKey = "WGXvLvr3mmG1bUwAfxAJfnZB5AUGMvGK"
+//let apiKey = "RVdi5NBIUK9GIdSTYUMGFSKg4GcppCpa"
 let locationSearchURL = "https://dataservice.accuweather.com/locations/v1/cities/autocomplete?"
 let currentConditionURL = "https://dataservice.accuweather.com/currentconditions/v1/"
